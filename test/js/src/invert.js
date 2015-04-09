0a911755a7c80ff4ca436850f199876c6d720501
aureooms <aurelien.ooms@gmail.com>
2015-04-09 11:01:21 +0200

    test invert

---
+++ test/js/src/invert.js
@@ -0,0 +1,24 @@
+
+var random = require( "aureooms-js-random" ) ;
+
+test( "invert" , function ( ) {
+
+	var m , n , rho , sigma , tau ;
+
+	m = 101 ;
+
+	for ( n = 0 ; n < m ; ++n ) {
+
+		sigma = permutation.identity( n ) ;
+
+		random.shuffle( sigma , 0 , n ) ;
+
+		tau = permutation.invert( sigma ) ;
+
+		rho = permutation.compose( sigma , tau ) ;
+
+		deepEqual( rho , permutation.identity( n ) , n + " : sigma * tau = id" ) ;
+
+	}
+
+} ) ;
