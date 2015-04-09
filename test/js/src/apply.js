1167c367bde6109cfa66928b502b4ddc7f55a08f
aureooms <aurelien.ooms@gmail.com>
2015-04-09 11:18:22 +0200

    test apply and transpositions

---
+++ test/js/src/apply.js
@@ -0,0 +1,22 @@
+
+var random = require( "aureooms-js-random" ) ;
+
+test( "apply" , function ( ) {
+
+	var m , n , rho , sigma , tau ;
+
+	m = 101 ;
+
+	for ( n = 0 ; n < m ; ++n ) {
+
+		sigma = permutation.identity( n ) ;
+
+		random.shuffle( sigma , 0 , n ) ;
+
+		tau = permutation.apply( sigma.length , permutation.transpositions( sigma ) ) ;
+
+		deepEqual( tau , sigma , n + " : sigma = tau" ) ;
+
+	}
+
+} ) ;
