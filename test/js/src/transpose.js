0e60d908a655f1983f5faed0dad0363b5c6119fb
aureooms <aurelien.ooms@gmail.com>
2015-04-09 11:47:13 +0200

    coverage for transpose and transposition

---
+++ test/js/src/transpose.js
@@ -0,0 +1,40 @@
+
+var random = require( "aureooms-js-random" ) ;
+
+test( "transpose" , function ( ) {
+
+	var m , n , rho , sigma , tau , upsilon , a , b ;
+
+	m = 101 ;
+
+	for ( n = 1 ; n < m ; ++n ) {
+
+		sigma = permutation.identity( n ) ;
+
+		random.shuffle( sigma , 0 , n ) ;
+
+		a = random.randint( 0 , n ) ;
+
+		b = random.randint( 0 , n ) ;
+
+		tau = permutation.transpose( sigma , a , b ) ;
+
+		rho = permutation.identity( n ) ;
+
+		random.shuffle( rho , 0 , n ) ;
+
+		upsilon = permutation.copy( rho ) ;
+
+		rho = permutation.compose( rho , sigma ) ;
+		upsilon = permutation.compose( upsilon , tau ) ;
+
+		deepEqual( rho[a] , upsilon[b] , n + " : rho[a] is upsilon[b]" ) ;
+		deepEqual( rho[b] , upsilon[a] , n + " : rho[b] is upsilon[a]" ) ;
+
+		upsilon = permutation.transpose( upsilon , a , b ) ;
+
+		deepEqual( upsilon , rho , n + " : upsilon is tau after transpose" ) ;
+
+	}
+
+} ) ;
