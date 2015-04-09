0f727a97f673aedd3bf3283c4fa4170d3ba9bc66
aureooms <aurelien.ooms@gmail.com>
2015-04-09 11:01:21 +0200

    test invert

--- js/dist/permutation.js
+++ js/dist/permutation.js
@@ -1,8 +1,696 @@
-(function(exports, undefined){
+'use strict';
+
+var _slicedToArray = function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } };
+
+(function (exports, undefined) {
 
 	'use strict';
 
+	/* js/src/_apply.js */
 
-/* js/src/dummy.js */
+	var _apply = function _apply(transpositions, sigma) {
+		var _iteratorNormalCompletion = true;
+		var _didIteratorError = false;
+		var _iteratorError = undefined;
 
-})(typeof exports === 'undefined' ? this['permutation'] = {} : exports);
+		try {
+
+			for (var _iterator = transpositions[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+				var _step$value = _slicedToArray(_step.value, 2);
+
+				var s = _step$value[0];
+				var t = _step$value[1];
+
+				_transpose(s, t, sigma);
+			}
+		} catch (err) {
+			_didIteratorError = true;
+			_iteratorError = err;
+		} finally {
+			try {
+				if (!_iteratorNormalCompletion && _iterator['return']) {
+					_iterator['return']();
+				}
+			} finally {
+				if (_didIteratorError) {
+					throw _iteratorError;
+				}
+			}
+		}
+	};
+
+	exports._apply = _apply;
+
+	/* js/src/_compose.js */
+
+	var _compose = regeneratorRuntime.mark(function _compose(sigma, tau) {
+		var _iteratorNormalCompletion2, _didIteratorError2, _iteratorError2, _iterator2, _step2, t;
+
+		return regeneratorRuntime.wrap(function _compose$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					_iteratorNormalCompletion2 = true;
+					_didIteratorError2 = false;
+					_iteratorError2 = undefined;
+					context$2$0.prev = 3;
+					_iterator2 = tau[Symbol.iterator]();
+
+				case 5:
+					if (_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done) {
+						context$2$0.next = 12;
+						break;
+					}
+
+					t = _step2.value;
+					context$2$0.next = 9;
+					return sigma[t];
+
+				case 9:
+					_iteratorNormalCompletion2 = true;
+					context$2$0.next = 5;
+					break;
+
+				case 12:
+					context$2$0.next = 18;
+					break;
+
+				case 14:
+					context$2$0.prev = 14;
+					context$2$0.t0 = context$2$0['catch'](3);
+					_didIteratorError2 = true;
+					_iteratorError2 = context$2$0.t0;
+
+				case 18:
+					context$2$0.prev = 18;
+					context$2$0.prev = 19;
+
+					if (!_iteratorNormalCompletion2 && _iterator2['return']) {
+						_iterator2['return']();
+					}
+
+				case 21:
+					context$2$0.prev = 21;
+
+					if (!_didIteratorError2) {
+						context$2$0.next = 24;
+						break;
+					}
+
+					throw _iteratorError2;
+
+				case 24:
+					return context$2$0.finish(21);
+
+				case 25:
+					return context$2$0.finish(18);
+
+				case 26:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, _compose, this, [[3, 14, 18, 26], [19,, 21, 25]]);
+	});
+
+	exports._compose = _compose;
+
+	/* js/src/_copy.js */
+
+	var _copy = function _copy(sigma, n, tau) {
+
+		for (var i = 0; i < n; ++i) {
+
+			tau[i] = sigma[i];
+		}
+	};
+
+	exports._copy = _copy;
+
+	/* js/src/_cycles.js */
+
+	var _cycles = regeneratorRuntime.mark(function _cycles(sigma, used) {
+		var _iteratorNormalCompletion3, _didIteratorError3, _iteratorError3, _iterator3, _step3, s, image, cycle;
+
+		return regeneratorRuntime.wrap(function _cycles$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					_iteratorNormalCompletion3 = true;
+					_didIteratorError3 = false;
+					_iteratorError3 = undefined;
+					context$2$0.prev = 3;
+					_iterator3 = sigma[Symbol.iterator]();
+
+				case 5:
+					if (_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done) {
+						context$2$0.next = 18;
+						break;
+					}
+
+					s = _step3.value;
+
+					if (!used[s]) {
+						context$2$0.next = 9;
+						break;
+					}
+
+					return context$2$0.abrupt('continue', 15);
+
+				case 9:
+
+					used[s] = true;
+
+					image = sigma[s];
+					cycle = [];
+
+					while (image !== s) {
+
+						used[image] = true;
+
+						cycle.push(image);
+
+						image = sigma[image];
+					}
+
+					context$2$0.next = 15;
+					return [s, cycle];
+
+				case 15:
+					_iteratorNormalCompletion3 = true;
+					context$2$0.next = 5;
+					break;
+
+				case 18:
+					context$2$0.next = 24;
+					break;
+
+				case 20:
+					context$2$0.prev = 20;
+					context$2$0.t1 = context$2$0['catch'](3);
+					_didIteratorError3 = true;
+					_iteratorError3 = context$2$0.t1;
+
+				case 24:
+					context$2$0.prev = 24;
+					context$2$0.prev = 25;
+
+					if (!_iteratorNormalCompletion3 && _iterator3['return']) {
+						_iterator3['return']();
+					}
+
+				case 27:
+					context$2$0.prev = 27;
+
+					if (!_didIteratorError3) {
+						context$2$0.next = 30;
+						break;
+					}
+
+					throw _iteratorError3;
+
+				case 30:
+					return context$2$0.finish(27);
+
+				case 31:
+					return context$2$0.finish(24);
+
+				case 32:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, _cycles, this, [[3, 20, 24, 32], [25,, 27, 31]]);
+	});
+
+	exports._cycles = _cycles;
+
+	/* js/src/_identity.js */
+
+	var _identity = function _identity(sigma, n) {
+
+		for (var i = 0; i < n; ++i) {
+
+			sigma[i] = i;
+		}
+	};
+
+	exports._identity = _identity;
+
+	/* js/src/_invert.js */
+
+	var _invert = function _invert(cycles, tau) {
+		var _iteratorNormalCompletion4 = true;
+		var _didIteratorError4 = false;
+		var _iteratorError4 = undefined;
+
+		try {
+
+			for (var _iterator4 = cycles[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+				var _step4$value = _slicedToArray(_step4.value, 2);
+
+				var s = _step4$value[0];
+				var cycle = _step4$value[1];
+				var _iteratorNormalCompletion5 = true;
+				var _didIteratorError5 = false;
+				var _iteratorError5 = undefined;
+
+				try {
+
+					for (var _iterator5 = cycle[Symbol.iterator](), _step5; !(_iteratorNormalCompletion5 = (_step5 = _iterator5.next()).done); _iteratorNormalCompletion5 = true) {
+						var t = _step5.value;
+
+						_transpose(tau, s, t);
+					}
+				} catch (err) {
+					_didIteratorError5 = true;
+					_iteratorError5 = err;
+				} finally {
+					try {
+						if (!_iteratorNormalCompletion5 && _iterator5['return']) {
+							_iterator5['return']();
+						}
+					} finally {
+						if (_didIteratorError5) {
+							throw _iteratorError5;
+						}
+					}
+				}
+			}
+		} catch (err) {
+			_didIteratorError4 = true;
+			_iteratorError4 = err;
+		} finally {
+			try {
+				if (!_iteratorNormalCompletion4 && _iterator4['return']) {
+					_iterator4['return']();
+				}
+			} finally {
+				if (_didIteratorError4) {
+					throw _iteratorError4;
+				}
+			}
+		}
+	};
+
+	exports._invert = _invert;
+
+	/* js/src/_itranspositions.js */
+
+	var _itranspositions = regeneratorRuntime.mark(function _itranspositions(sigma, used) {
+		var _iteratorNormalCompletion6, _didIteratorError6, _iteratorError6, _iterator6, _step6, s, image;
+
+		return regeneratorRuntime.wrap(function _itranspositions$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					_iteratorNormalCompletion6 = true;
+					_didIteratorError6 = false;
+					_iteratorError6 = undefined;
+					context$2$0.prev = 3;
+					_iterator6 = sigma[Symbol.iterator]();
+
+				case 5:
+					if (_iteratorNormalCompletion6 = (_step6 = _iterator6.next()).done) {
+						context$2$0.next = 21;
+						break;
+					}
+
+					s = _step6.value;
+
+					if (!used[s]) {
+						context$2$0.next = 9;
+						break;
+					}
+
+					return context$2$0.abrupt('continue', 18);
+
+				case 9:
+
+					used[s] = true;
+
+					image = sigma[s];
+
+				case 11:
+					if (!(image !== s)) {
+						context$2$0.next = 18;
+						break;
+					}
+
+					used[image] = true;
+
+					context$2$0.next = 15;
+					return [s, image];
+
+				case 15:
+
+					image = sigma[image];
+
+					context$2$0.next = 11;
+					break;
+
+				case 18:
+					_iteratorNormalCompletion6 = true;
+					context$2$0.next = 5;
+					break;
+
+				case 21:
+					context$2$0.next = 27;
+					break;
+
+				case 23:
+					context$2$0.prev = 23;
+					context$2$0.t2 = context$2$0['catch'](3);
+					_didIteratorError6 = true;
+					_iteratorError6 = context$2$0.t2;
+
+				case 27:
+					context$2$0.prev = 27;
+					context$2$0.prev = 28;
+
+					if (!_iteratorNormalCompletion6 && _iterator6['return']) {
+						_iterator6['return']();
+					}
+
+				case 30:
+					context$2$0.prev = 30;
+
+					if (!_didIteratorError6) {
+						context$2$0.next = 33;
+						break;
+					}
+
+					throw _iteratorError6;
+
+				case 33:
+					return context$2$0.finish(30);
+
+				case 34:
+					return context$2$0.finish(27);
+
+				case 35:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, _itranspositions, this, [[3, 23, 27, 35], [28,, 30, 34]]);
+	});
+
+	exports._itranspositions = _itranspositions;
+
+	/* js/src/_transpose.js */
+
+	var _transpose = function _transpose(a, b, sigma) {
+
+		var tmp = sigma[a];
+		sigma[a] = sigma[b];
+		sigma[b] = tmp;
+	};
+
+	exports._transpose = _transpose;
+
+	/* js/src/_transposition.js */
+
+	var _transposition = function _transposition(a, b, sigma) {
+
+		sigma[a] = b;
+		sigma[b] = a;
+	};
+
+	exports._transposition = _transposition;
+
+	/* js/src/_transpositions.js */
+	var _transpositions = regeneratorRuntime.mark(function _transpositions(cycles) {
+		var _iteratorNormalCompletion7, _didIteratorError7, _iteratorError7, _iterator7, _step7, _step7$value, s, cycle;
+
+		return regeneratorRuntime.wrap(function _transpositions$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					_iteratorNormalCompletion7 = true;
+					_didIteratorError7 = false;
+					_iteratorError7 = undefined;
+					context$2$0.prev = 3;
+					_iterator7 = cycles[Symbol.iterator]();
+
+				case 5:
+					if (_iteratorNormalCompletion7 = (_step7 = _iterator7.next()).done) {
+						context$2$0.next = 17;
+						break;
+					}
+
+					_step7$value = _slicedToArray(_step7.value, 2);
+					s = _step7$value[0];
+					cycle = _step7$value[1];
+
+				case 9:
+					if (!cycle.length) {
+						context$2$0.next = 14;
+						break;
+					}
+
+					context$2$0.next = 12;
+					return [s, cycle.pop()];
+
+				case 12:
+					context$2$0.next = 9;
+					break;
+
+				case 14:
+					_iteratorNormalCompletion7 = true;
+					context$2$0.next = 5;
+					break;
+
+				case 17:
+					context$2$0.next = 23;
+					break;
+
+				case 19:
+					context$2$0.prev = 19;
+					context$2$0.t3 = context$2$0['catch'](3);
+					_didIteratorError7 = true;
+					_iteratorError7 = context$2$0.t3;
+
+				case 23:
+					context$2$0.prev = 23;
+					context$2$0.prev = 24;
+
+					if (!_iteratorNormalCompletion7 && _iterator7['return']) {
+						_iterator7['return']();
+					}
+
+				case 26:
+					context$2$0.prev = 26;
+
+					if (!_didIteratorError7) {
+						context$2$0.next = 29;
+						break;
+					}
+
+					throw _iteratorError7;
+
+				case 29:
+					return context$2$0.finish(26);
+
+				case 30:
+					return context$2$0.finish(23);
+
+				case 31:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, _transpositions, this, [[3, 19, 23, 31], [24,, 26, 30]]);
+	});
+
+	exports._transpositions = _transpositions;
+
+	/* js/src/_used.js */
+
+	var _used = function _used(n, array) {
+
+		for (var i = 0; i < n; ++i) {
+			array[i] = false;
+		}
+	};
+
+	exports._used = _used;
+
+	/* js/src/apply.js */
+
+	var apply = function apply(n, transpositions) {
+
+		var rho = identity(n);
+
+		_apply(transpositions, rho);
+
+		return rho;
+	};
+
+	exports.apply = apply;
+
+	/* js/src/compose.js */
+
+	var compose = function compose(sigma, tau) {
+
+		var rho = permutation(sigma.length);
+
+		var i = 0;
+
+		var _iteratorNormalCompletion8 = true;
+		var _didIteratorError8 = false;
+		var _iteratorError8 = undefined;
+
+		try {
+			for (var _iterator8 = _compose(sigma, tau)[Symbol.iterator](), _step8; !(_iteratorNormalCompletion8 = (_step8 = _iterator8.next()).done); _iteratorNormalCompletion8 = true) {
+				var r = _step8.value;
+
+				rho[i] = r;
+				++i;
+			}
+		} catch (err) {
+			_didIteratorError8 = true;
+			_iteratorError8 = err;
+		} finally {
+			try {
+				if (!_iteratorNormalCompletion8 && _iterator8['return']) {
+					_iterator8['return']();
+				}
+			} finally {
+				if (_didIteratorError8) {
+					throw _iteratorError8;
+				}
+			}
+		}
+
+		return rho;
+	};
+
+	exports.compose = compose;
+
+	/* js/src/copy.js */
+
+	var copy = function copy(sigma) {
+
+		var rho = permutation(sigma.length);
+
+		_copy(sigma, sigma.length, rho);
+
+		return rho;
+	};
+
+	exports.copy = copy;
+
+	/* js/src/cycles.js */
+
+	var cycles = regeneratorRuntime.mark(function cycles(sigma) {
+		return regeneratorRuntime.wrap(function cycles$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					return context$2$0.delegateYield(_cycles(sigma, used(sigma.length)), 't4', 1);
+
+				case 1:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, cycles, this);
+	});
+
+	exports.cycles = cycles;
+
+	/* js/src/identity.js */
+
+	var identity = function identity(n) {
+
+		var rho = permutation(n);
+
+		_identity(rho, n);
+
+		return rho;
+	};
+
+	exports.identity = identity;
+
+	/* js/src/invert.js */
+
+	var invert = function invert(sigma) {
+
+		return apply(sigma.length, itranspositions(sigma));
+	};
+
+	exports.invert = invert;
+
+	/* js/src/itranspositions.js */
+
+	var itranspositions = regeneratorRuntime.mark(function itranspositions(sigma) {
+		return regeneratorRuntime.wrap(function itranspositions$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					return context$2$0.delegateYield(_itranspositions(sigma, used(sigma.length)), 't5', 1);
+
+				case 1:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, itranspositions, this);
+	});
+
+	exports.itranspositions = itranspositions;
+
+	/* js/src/permutation.js */
+
+	var permutation = function permutation(n) {
+
+		return new Array(n);
+	};
+
+	exports.permutation = permutation;
+
+	/* js/src/transpose.js */
+
+	var transpose = function transpose(sigma, a, b) {
+
+		var rho = copy(sigma);
+
+		_transpose(a, b, rho);
+
+		return rho;
+	};
+
+	exports.transpose = transpose;
+
+	/* js/src/transposition.js */
+
+	var transposition = function transposition(n, a, b) {
+
+		var rho = identity(n);
+
+		_transposition(a, b, rho);
+
+		return rho;
+	};
+
+	exports.transposition = transposition;
+
+	/* js/src/transpositions.js */
+
+	var transpositions = regeneratorRuntime.mark(function transpositions(sigma) {
+		return regeneratorRuntime.wrap(function transpositions$(context$2$0) {
+			while (1) switch (context$2$0.prev = context$2$0.next) {
+				case 0:
+					return context$2$0.delegateYield(_transpositions(cycles(sigma, used(sigma.length))), 't6', 1);
+
+				case 1:
+				case 'end':
+					return context$2$0.stop();
+			}
+		}, transpositions, this);
+	});
+
+	exports.transpositions = transpositions;
+
+	/* js/src/used.js */
+
+	var used = function used(n) {
+
+		var array = new Array(n);
+
+		_used(n, array);
+
+		return array;
+	};
+
+	exports.used = used;
+})(typeof exports === 'undefined' ? undefined.permutation = {} : exports);
