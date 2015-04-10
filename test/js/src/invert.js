fc08ebae849f4b1b68a4e543fcd39c1742db438c
aureooms <aurelien.ooms@gmail.com>
2015-04-10 10:01:25 +0200

    more generic inversion algorithm testing

--- test/js/src/invert.js
+++ test/js/src/invert.js
@@ -1,24 +1,57 @@
 
 var random = require( "aureooms-js-random" ) ;
 
-test( "invert" , function ( ) {
+var t = function ( name , invert ) {
 
-	var m , n , rho , sigma , tau ;
+	test( "invert > " + name , function ( ) {
 
-	m = 100 ;
+		var m , n , rho , sigma , tau ;
 
-	for ( n = 0 ; n < m ; ++n ) {
+		m = 100 ;
 
-		sigma = permutation.identity( n ) ;
+		for ( n = 0 ; n < m ; ++n ) {
 
-		random.shuffle( sigma , 0 , n ) ;
+			sigma = permutation.identity( n ) ;
 
-		tau = permutation.invert( sigma ) ;
+			random.shuffle( sigma , 0 , n ) ;
 
-		rho = permutation.compose( sigma , tau ) ;
+			tau = invert( sigma ) ;
 
-		deepEqual( rho , permutation.identity( n ) , n + " : sigma * tau = id" ) ;
+			rho = permutation.compose( sigma , tau ) ;
 
-	}
+			deepEqual( rho , permutation.identity( n ) , n + " : sigma * tau = id" ) ;
+
+		}
+
+	} ) ;
+
+} ;
+
+var algorithms = [
+
+	{ name : "invert" , invert : permutation.invert } ,
+	{ name : "_invertcycles" , invert : function ( sigma ) {
+
+		var tau = permutation.identity( sigma.length ) ;
+
+		var cycles = permutation.cycles( sigma ) ;
+
+		permutation._invertcycles( cycles , tau ) ;
+
+		return tau ;
+
+	} } ,
+
+	{ name : "apply( itranspositions )" , invert : function ( sigma ) {
+
+		return permutation.apply( sigma.length , permutation.itranspositions( sigma ) ) ;
+
+	} }
+
+] ;
+
+algorithms.forEach( function ( algorithm ) {
+
+	t( algorithm.name , algorithm.invert ) ;
 
 } ) ;
