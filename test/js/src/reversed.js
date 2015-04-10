321d569cfdb3611f4f3af9c0acac4d016dc3e557
aureooms <aurelien.ooms@gmail.com>
2015-04-10 11:51:37 +0200

    test reversed

---
+++ test/js/src/reversed.js
@@ -0,0 +1,22 @@
+
+var random = require( "aureooms-js-random" ) ;
+
+test( "reversed" , function ( ) {
+
+	var m , n , sigma , tau ;
+
+	m = 100 ;
+
+	for ( n = 0 ; n < m ; ++n ) {
+
+		sigma = permutation.identity( n ) ;
+
+		random.shuffle( sigma , 0 , n ) ;
+
+		tau = permutation.reversed( sigma ) ;
+
+		deepEqual( tau.reverse( ) , sigma , n + " : reversed( tau ) = sigma" ) ;
+
+	}
+
+} ) ;
