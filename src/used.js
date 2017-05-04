cf350b6ebc2427af5049cd7ea4adecbeccc83765
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-05-04 03:36:34 +0200

    docs:
    missing types

--- src/used.js
+++ src/used.js
@@ -3,7 +3,7 @@ import { _used } from './_used' ;
 /**
  * Generates an helper array of given size (used in {@link _cycle}, {@link cycle}).
  *
- * @param n The given size.
+ * @param {Number} n The given size.
  * @returns {Array} The helper array of prescribed size.
  */
 export function used ( n ) {
